b8d67e557ddf9b1b61ede0793e1885a126a3dc6d
pedrinhosj <joaopsj007@gmail.com>
2023-12-12 22:23:41 -0300

    refactor: sequelize instance use env variable

--- src/app/db/sequelize/sequelize.ts
+++ src/app/db/sequelize/sequelize.ts
@@ -1,5 +1,8 @@
+/* eslint-disable import/first */
 /* eslint-disable @typescript-eslint/no-var-requires */
+import dotenv from 'dotenv'
+dotenv.config()
 import { Sequelize } from 'sequelize'
 const config = require('./config');
 
-export const sequelize = new Sequelize(config.development)
+export const sequelize = new Sequelize(config[process.env.NODE_ENV])
