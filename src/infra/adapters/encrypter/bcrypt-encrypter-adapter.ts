d5ecbe9d940257842f680a24464e57eaf19e2106
pedrinhosj <joaopsj007@gmail.com>
2023-12-12 21:37:33 -0300

    feat: create BcryptEncrypterAdapter for Encrypter port

---
+++ src/infra/adapters/encrypter/bcrypt-encrypter-adapter.ts
@@ -0,0 +1,10 @@
+import bcrypt from 'bcrypt'
+import { type Encrypter } from '../../../domain/ports/encrypter'
+
+export class BcryptEncrypterAdapter implements Encrypter {
+  constructor (private readonly salt: number) {}
+  async encrypt (value: string): Promise<string> {
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
+  }
+}
