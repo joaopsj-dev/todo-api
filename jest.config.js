51a9011d9d527944cf2173218b4418d5abe90169
pedrinhosj <joaopsj007@gmail.com>
2023-12-15 18:41:11 -0300

    chore: ignore test coverage server listen file

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/**/protocols/**',
     '!<rootDir>/src/**/*protocols.ts',
+    '!<rootDir>/src/app/server/index.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
