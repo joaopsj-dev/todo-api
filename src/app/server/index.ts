8a278d1a52a47f2ffc79d2d60b457e67289049cb
pedrinhosj <joaopsj007@gmail.com>
2023-12-14 22:52:22 -0300

    feat: create server listen

---
+++ src/app/server/index.ts
@@ -0,0 +1,8 @@
+import { sequelize } from '../db/sequelize/sequelize'
+
+sequelize.authenticate()
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(process.env.PORT, () => console.log(`Server running at http://localhost:${process.env.PORT}`))
+  })
+  .catch(console.error)
