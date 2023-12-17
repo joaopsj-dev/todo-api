66131865fe74d5ce7a5cafee29a0dee2f43a0f8e
pedrinhosj <joaopsj007@gmail.com>
2023-12-17 17:11:45 -0300

    feat: create Authenticate usecase

---
+++ src/domain/usecases/authenticate/authenticate-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../domain/entities/account'
+export * from '../../../domain/ports/account-repository'
+export * from '../../../domain/ports/encrypter'
+export * from './authenticate'
