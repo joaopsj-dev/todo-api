f24a0c16538e7aaf4a3667f448d64437262fe83c
pedrinhosj <joaopsj007@gmail.com>
2023-12-17 18:09:49 -0300

    feat: create LoginController

---
+++ src/presentation/controllers/login/login-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../protocols/index'
+export * from '../../../domain/usecases/authenticate/authenticate'
+export * from '../../../domain/ports/token'
+export * from '../../../domain/ports/validate'
