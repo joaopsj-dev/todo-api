9c8283c0363927fd7a1a7ca2c1514a6a007d767e
pedrinhosj <joaopsj007@gmail.com>
2024-01-04 14:28:58 -0300

    test: ensure findById and update methods operation of AccountSequelizeRepository

--- src/infra/adapters/repositories/account/sequelize-repository.spec.ts
+++ src/infra/adapters/repositories/account/sequelize-repository.spec.ts
@@ -6,8 +6,9 @@ const makeSut = (): SequelizeAccountRepositoryAdapter => {
 }
 
 describe('Account Sequelize Repository', () => {
-  test('Should return an account on success', async () => {
+  test('Should return an account on create method success', async () => {
     const sut = makeSut()
+
     const account = await sut.create({ id: 'valid_id', refreshToken: 'valid_refreshToken', name: 'valid_name', email: 'valid_email', password: 'valid_password' })
 
     expect(account).toEqual(expect.objectContaining({
@@ -21,10 +22,33 @@ describe('Account Sequelize Repository', () => {
     }))
   })
 
-  test('Should return an account with correct email', async () => {
+  test('Should return an account with correct email on findByEmail method success', async () => {
     const sut = makeSut()
+
     await sut.create({ id: 'valid_id', refreshToken: 'valid_refreshToken', name: 'valid_name', email: 'valid_email', password: 'valid_password' })
     const accountByEmail = await sut.findByEmail('valid_email')
+
     expect(accountByEmail.email).toBe('valid_email')
+  })
+
+  test('Should return an account with correct id on findById method success', async () => {
+    const sut = makeSut()
+
+    await sut.create({ id: 'valid_id', refreshToken: 'valid_refreshToken', name: 'valid_name', email: 'valid_email', password: 'valid_password' })
+    const accountById = await sut.findById('valid_id')
+
+    expect(accountById.id).toBe('valid_id')
+  })
+
+  test('Should update and return an account on update method success', async () => {
+    const sut = makeSut()
+
+    await sut.create({ id: 'valid_id', refreshToken: 'valid_refreshToken', name: 'valid_name', email: 'valid_email', password: 'valid_password' })
+    const updatedAccount = await sut.update(
+      { id: 'valid_id', refreshToken: 'valid_refreshToken', name: 'new_name', email: 'valid_email', password: 'valid_password' },
+      'valid_id'
+    )
+
+    expect(updatedAccount.name).toBe('new_name')
   })
 })
