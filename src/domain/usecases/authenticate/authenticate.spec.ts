ae8469da605e7cdec025d337856c0b581cbd345d
pedrinhosj <joaopsj007@gmail.com>
2024-01-04 12:10:14 -0300

    refactor: authenticate test

--- src/domain/usecases/authenticate/authenticate.spec.ts
+++ src/domain/usecases/authenticate/authenticate.spec.ts
@@ -1,17 +1,17 @@
-import { type Failure, type Success } from '../../protocols/either'
-import { type Account, type AccountRepository, Authenticate, type AuthenticateError, type Encrypter, type Token } from './authenticate-protocols'
+import { Authenticate, type Account, type AccountRepository, type AuthenticateError, type Encrypter, type Token, type ParseTokenError, type TokenPayload } from './authenticate-protocols'
+import { type Either, type Failure, type Success } from '../../protocols/either'
 
 const makeFakeAccount = (): Account => ({
   id: 'any_id',
   refreshToken: 'any_refresh_token',
   name: 'any_name',
   email: 'any_email',
-  password: 'hashed_password'
+  password: 'any_password'
 })
 
 const makeFakeAuthenticateData = (): { email: string, password: string } => ({
-  email: 'any_email',
-  password: 'any_password'
+  email: 'valid_email',
+  password: 'valid_password'
 })
 
 const makeAccountRepository = (): AccountRepository => {
@@ -20,30 +20,23 @@ const makeAccountRepository = (): AccountRepository => {
       return new Promise(resolve => resolve(makeFakeAccount()))
     }
 
-    async findById (): Promise<Account> {
-      return new Promise(resolve => resolve(makeFakeAccount()))
-    }
-
-    async create (): Promise<Account> {
-      return new Promise(resolve => resolve(makeFakeAccount()))
-    }
-
     async update (): Promise<Account> {
       return new Promise(resolve => resolve(makeFakeAccount()))
     }
+
+    findById: (id: string) => Promise<Account>
+    create: (accountData: Account) => Promise<Account>
   }
   return new AccountRepositoryStub()
 }
 
 const makeEncrypter = (): Encrypter => {
   class EncryptStub implements Encrypter {
-    async encrypt (data: string): Promise<string> {
-      return new Promise(resolve => resolve('hashed_password'))
-    }
-
     async parse (data: string, encrypted: string): Promise<boolean> {
       return new Promise(resolve => resolve(true))
     }
+
+    encrypt: (data: string) => Promise<string>
   }
   return new EncryptStub()
 }
@@ -54,9 +47,7 @@ const makeTokenProvider = (): Token => {
       return new Promise(resolve => resolve('token'))
     }
 
-    async parse (): Promise<any> {
-      return new Promise(resolve => resolve(null))
-    }
+    parse: (token: string, secretKey: string) => Promise<Either<ParseTokenError, TokenPayload>>
   }
   return new TokenProviderStub()
 }
@@ -84,42 +75,40 @@ type SuccessByAuthenticate = Success<null, Account>
 type FailureByAuthenticate = Failure<AuthenticateError, null>
 
 describe('Authenticate UseCase', () => {
-  test('Should return a correct error if there is no user with the email provided', async () => {
-    const { sut, accountRepositoryStub } = makeSut()
-
-    jest.spyOn(accountRepositoryStub, 'findByEmail').mockReturnValueOnce(new Promise(resolve => resolve(null)))
-    const response = await sut.auth(makeFakeAuthenticateData()) as FailureByAuthenticate
-    expect(response.error.message).toBe('user not found')
-  })
-
-  test('Should return a correct error if incorrect password', async () => {
-    const { sut, encrypterStub } = makeSut()
-
-    jest.spyOn(encrypterStub, 'parse').mockReturnValueOnce(new Promise(resolve => resolve(false)))
-    const response = await sut.auth(makeFakeAuthenticateData()) as FailureByAuthenticate
-    expect(response.error.message).toBe('incorrect password')
-  })
-
   test('Should call findByEmail with correct e-mail', async () => {
     const { sut, accountRepositoryStub } = makeSut()
 
     const findByEmailSpy = jest.spyOn(accountRepositoryStub, 'findByEmail')
     await sut.auth(makeFakeAuthenticateData())
-    expect(findByEmailSpy).toHaveBeenCalledWith('any_email')
-  })
 
-  test('Should call Encrypter parse method with correct password', async () => {
-    const { sut, encrypterStub } = makeSut()
-    const encryptSpy = jest.spyOn(encrypterStub, 'parse')
-    await sut.auth(makeFakeAuthenticateData())
-    expect(encryptSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
+    expect(findByEmailSpy).toHaveBeenCalledWith('valid_email')
   })
 
   test('Should throw AccountRepository throws', async () => {
     const { sut, accountRepositoryStub } = makeSut()
+
     jest.spyOn(accountRepositoryStub, 'findByEmail').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.auth(makeFakeAuthenticateData())
+
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return a failure if there is no user with the email provided', async () => {
+    const { sut, accountRepositoryStub } = makeSut()
+
+    jest.spyOn(accountRepositoryStub, 'findByEmail').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const response = await sut.auth(makeFakeAuthenticateData()) as FailureByAuthenticate
+
+    expect(response.error.message).toStrictEqual(expect.any(String))
+  })
+
+  test('Should call Encrypter parse method with correct password', async () => {
+    const { sut, encrypterStub } = makeSut()
+
+    const encryptSpy = jest.spyOn(encrypterStub, 'parse')
+    await sut.auth(makeFakeAuthenticateData())
+
+    expect(encryptSpy).toHaveBeenCalledWith('valid_password', 'any_password')
   })
 
   test('Should throw Encrypter throws', async () => {
@@ -129,9 +118,20 @@ describe('Authenticate UseCase', () => {
     await expect(promise).rejects.toThrow()
   })
 
-  test('Should return an account if on success', async () => {
+  test('Should return a failure if incorrect password', async () => {
+    const { sut, encrypterStub } = makeSut()
+
+    jest.spyOn(encrypterStub, 'parse').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    const response = await sut.auth(makeFakeAuthenticateData()) as FailureByAuthenticate
+
+    expect(response.error.message).toStrictEqual(expect.any(String))
+  })
+
+  test('Should return a account if on success', async () => {
     const { sut } = makeSut()
+
     const { response: account } = await sut.auth(makeFakeAuthenticateData()) as SuccessByAuthenticate
+
     expect(account).toEqual(makeFakeAccount())
   })
 })
