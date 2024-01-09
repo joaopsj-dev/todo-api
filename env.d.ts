0f30d4a55fcef30960eb7b386b9377445bd4d88a
pedrinhosj <joaopsj007@gmail.com>
2024-01-08 22:01:30 -0300

    chore: add nodemailer

--- env.d.ts
+++ env.d.ts
@@ -5,6 +5,11 @@ declare global {
       NODE_ENV: string
       ACCESS_TOKEN_SECRET_KEY: string
       REFRESH_TOKEN_SECRET_KEY: string
+      MAIL_HOST: string
+      MAIL_PORT: string
+      MAIL_USER: string
+      MAIL_PASSWORD: string
+      MAIL_FROM: string
     }
   }
 }
