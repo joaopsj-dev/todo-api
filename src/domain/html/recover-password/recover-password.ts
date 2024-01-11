e665c4ee59b87371d1a1ae1e611b434e1fb92bf6
pedrinhosj <joaopsj007@gmail.com>
2024-01-11 17:12:07 -0300

    refactor: change password recover email html

--- src/domain/html/recover-password/recover-password.ts
+++ src/domain/html/recover-password/recover-password.ts
@@ -1,10 +1,7 @@
-const dotenv = require('dotenv');
-dotenv.config()
 const fs = require('fs');
 import path from 'path';
 
 const recoverPasswordHtmlPath = path.join(__dirname, 'recover-password.html');
 const recoverPasswordHtmlTemplate = fs.readFileSync(recoverPasswordHtmlPath, 'utf8')
-  .replace('{PORT}', process.env.PORT);
 
 export default recoverPasswordHtmlTemplate
