aaee0332b12d0478f33b3e0a3224300507a68911
pedrinhosj <joaopsj007@gmail.com>
2024-01-12 12:29:33 -0300

    refactor: change transporter config

--- src/infra/email/config.ts
+++ src/infra/email/config.ts
@@ -3,9 +3,7 @@ dotenv.config()
 import nodemailer from 'nodemailer'
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: +process.env.MAIL_PORT,
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD
