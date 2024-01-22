c273197d7d16d0883f9e3158a865a425285332c9
pedrinhosj <joaopsj007@gmail.com>
2024-01-22 16:40:02 -0300

    feat: create CreateTaskController

---
+++ src/presentation/controllers/task/create-task.ts
@@ -0,0 +1,32 @@
+import { type Validator, type Controller, type HttpRequest, type HttpResponse, type CreateTask } from './create-task-protocols'
+import { badRequest, notFound, serverError, ok } from '../../helpers/http-helper'
+
+export class CreateTaskController implements Controller {
+  constructor (
+    private readonly validator: Validator,
+    private readonly createTask: CreateTask
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const validateResponse = await this.validator.validate(httpRequest.body)
+      if (validateResponse.isFailure()) {
+        return badRequest(validateResponse.error)
+      }
+
+      const createTaskResponse = await this.createTask.create(httpRequest.body)
+
+      if (createTaskResponse.isFailure()) {
+        const { message, type } = createTaskResponse.error
+
+        return type === 'AccountNotFound'
+          ? notFound({ message })
+          : badRequest({ message })
+      }
+
+      return ok({ task: createTaskResponse.response })
+    } catch (error) {
+      return serverError(error as any)
+    }
+  }
+}
