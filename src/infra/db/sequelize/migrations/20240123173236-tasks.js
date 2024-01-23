1ba7db91bcea5762355d515043838f2ebc9d6a57
pedrinhosj <joaopsj007@gmail.com>
2024-01-23 17:48:17 -0300

    feat: create Task sequelize migrate

---
+++ src/infra/db/sequelize/migrations/20240123173236-tasks.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      accountId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: 'accounts',
+          key: 'id',
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      notifyDate: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      endDate: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      isNotify: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      notification: {
+        allowNull: true,
+        type: Sequelize.JSON,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('tasks');
+  }
+};
