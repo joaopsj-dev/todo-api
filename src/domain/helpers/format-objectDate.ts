694eb321151a894d2e936a69557659a58c1f2ac6
pedrinhosj <joaopsj007@gmail.com>
2024-01-29 10:26:03 -0300

    refactor: format objectDate correctly

--- src/domain/helpers/format-objectDate.ts
+++ src/domain/helpers/format-objectDate.ts
@@ -1,3 +1,3 @@
 import { type ObjectDate } from '../entities/task'
 
-export const formatObjectDate = ({ year, month, day, hour, minute }: ObjectDate): Date => new Date(year, month, day, hour, minute)
+export const formatObjectDate = ({ year, month, day, hour, minute }: ObjectDate): Date => new Date(year, month - 1, day, hour, minute)
