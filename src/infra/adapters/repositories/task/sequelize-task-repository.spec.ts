6c039c0b75e4d9eba3b421604783c3956ff18f4c
pedrinhosj <joaopsj007@gmail.com>
2024-01-28 21:12:26 -0300

    test: ensure findByIsNotify method operation

--- src/infra/adapters/repositories/task/sequelize-task-repository.spec.ts
+++ src/infra/adapters/repositories/task/sequelize-task-repository.spec.ts
@@ -4,6 +4,9 @@ import AccountModel from '../../../db/sequelize/models/Account';
 import TaskModel from '../../../db/sequelize/models/Task';
 import { SequelizeTaskRepositoryAdapter } from './sequelize-task-repository';
 
+const notifyDate = new Date(Date.now() + 5000)
+const endDate = new Date(Date.now() + 10000)
+
 const makeFakeCreateAccountData = (): Account => ({
   id: 'valid_id',
   refreshToken: 'valid_refreshToken',
@@ -15,21 +18,20 @@ const makeFakeCreateAccountData = (): Account => ({
 const makeFakeCreateTaskData = async (): Promise<Task> => {
   try {
     const { id: accountId } = await AccountModel.create({ ...makeFakeCreateAccountData() }) as any
-    const a = {
+    return {
       id: 'any_id',
       accountId,
       name: 'any_name',
       description: 'any_description',
-      notifyDate: new Date(Date.now() + 5000),
-      endDate: new Date(Date.now() + 10000),
+      notifyDate: { year: notifyDate.getFullYear(), month: notifyDate.getMonth() + 1, day: notifyDate.getDate(), hour: notifyDate.getHours(), minute: notifyDate.getMinutes() },
+      endDate: { year: endDate.getFullYear(), month: endDate.getMonth() + 1, day: endDate.getDate(), hour: endDate.getHours(), minute: endDate.getMinutes() },
       isNotify: true,
       status: 'pending',
-      notification: 'any_description',
       createdAt: new Date(),
       updatedAt: new Date()
     }
-    return a as any
   } catch (error) {
+    console.log(error);
   }
 }
 
@@ -38,7 +40,7 @@ const makeSut = (): SequelizeTaskRepositoryAdapter => {
 }
 
 describe('Task Sequelize Repository', () => {
-  test('Should throws if sequelize create method throws', async () => {
+  test('Should throws if sequelize throws', async () => {
     const sut = makeSut()
 
     jest.spyOn(TaskModel, 'create').mockRejectedValueOnce(new Error())
@@ -50,21 +52,37 @@ describe('Task Sequelize Repository', () => {
   test('Should return an task on create method success', async () => {
     const sut = makeSut()
 
-    console.log(sut);
-    const task = await sut.create(await makeFakeCreateTaskData())
+    const createTaskData = await makeFakeCreateTaskData()
+    const task = await sut.create(createTaskData)
 
     expect(task).toEqual(expect.objectContaining({
-      id: 'any_id',
-      accountId: 'valid_id',
-      name: 'any_name',
-      description: 'any_description',
-      notifyDate: expect.any(Date),
-      endDate: expect.any(Date),
-      isNotify: true,
-      status: 'pending',
-      notification: 'any_description',
+      ...createTaskData,
       createdAt: expect.any(Date),
       updatedAt: expect.any(Date)
     }))
+  })
+
+  test('Should return all tasks that need to be notified', async () => {
+    const sut = makeSut()
+
+    const createTaskData = await makeFakeCreateTaskData()
+    await sut.create(createTaskData)
+    await sut.create({
+      ...createTaskData,
+      id: 'any_id_2',
+      isNotify: false
+    })
+    await sut.create({
+      ...createTaskData,
+      id: 'any_id_3',
+      notifyDate: { year: notifyDate.getFullYear(), month: notifyDate.getMonth() + 1, day: notifyDate.getDate() + 1, hour: notifyDate.getHours(), minute: notifyDate.getMinutes() }
+    })
+
+    const tasksByIsNotify = await sut.findByIsNotify()
+
+    expect(tasksByIsNotify.length).toBe(1)
+    tasksByIsNotify.forEach(task => {
+      expect(task.isNotify).toBe(true)
+    })
   })
 })
