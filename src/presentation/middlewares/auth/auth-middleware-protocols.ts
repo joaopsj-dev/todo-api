11094b089ecb99c6067b6f737a2c5596656686c6
pedrinhosj <joaopsj007@gmail.com>
2024-02-01 16:26:13 -0300

    feat: create AuthMiddleware

---
+++ src/presentation/middlewares/auth/auth-middleware-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../../domain/ports/token'
+export * from '../../protocols'
+export * from '../../protocols/middleware'
---
+++ src/presentation/middlewares/auth/auth-middleware.ts
@@ -0,0 +1,24 @@
+import { type Token, type HttpResponse, type HttpRequest, type Middleware } from './auth-middleware-protocols'
+import { forbidden, ok, serverError } from '../../helpers/http-helper';
+import token_protocols from '../../../domain/protocols/token';
+
+export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly token: Token
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      const tokenResponse = await this.token.parse(accessToken, token_protocols.accessToken_secret_key)
+
+      if (tokenResponse.isFailure()) {
+        return forbidden({ message: 'Access denied' })
+      }
+
+      return ok({})
+    } catch (error) {
+      return serverError(error as any)
+    }
+  }
+}
