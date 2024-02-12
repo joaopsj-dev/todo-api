39c0923ea7be52ebc08bac3ad37c9c13ab39065e
pedrinhosj <joaopsj007@gmail.com>
2024-02-12 12:28:40 -0300

    test: ensure RemoveAccount usecase operation

---
+++ src/domain/usecases/account/remove-account/remove-account.spec.ts
@@ -0,0 +1,93 @@
+import { type Account } from '../../../entities/account'
+import { type AccountRepository } from './remove-account-protocols'
+import { RemoveAccount } from './remove-account'
+
+const makeFakeAccount = (): Account => ({
+  id: 'any_id',
+  refreshToken: 'any_refreshToken',
+  accessToken: 'any_accessToken',
+  name: 'any_name',
+  email: 'any_email',
+  password: 'any_password',
+  createdAt: new Date(),
+  updatedAt: new Date()
+})
+
+const makeAccountRepository = (): AccountRepository => {
+  class AccountRepositoryStub implements AccountRepository {
+    async findById (): Promise<Account> {
+      return new Promise(resolve => resolve(makeFakeAccount()))
+    }
+
+    async delete (accountId: string): Promise<void> {
+      return new Promise(resolve => resolve())
+    }
+
+    create: () => Promise<Account>
+    update: () => Promise<Account>
+    findByEmail: () => Promise<Account>
+  }
+
+  return new AccountRepositoryStub()
+}
+
+interface SutTypes {
+  sut: RemoveAccount
+  accountRepositoryStub: AccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const accountRepositoryStub = makeAccountRepository()
+  const sut = new RemoveAccount(accountRepositoryStub)
+
+  return {
+    sut,
+    accountRepositoryStub
+  }
+}
+
+describe('RemoveAccount usecase', () => {
+  test('Should throw AccountRepository throws', async () => {
+    const { sut, accountRepositoryStub } = makeSut()
+
+    jest.spyOn(accountRepositoryStub, 'findById').mockRejectedValueOnce(new Error())
+    const promise = sut.remove('any_id')
+
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call findById with correct id', async () => {
+    const { sut, accountRepositoryStub } = makeSut()
+
+    const findByIdSpy = jest.spyOn(accountRepositoryStub, 'findById')
+    await sut.remove('any_id')
+
+    expect(findByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should return false if account by id not found', async () => {
+    const { sut, accountRepositoryStub } = makeSut()
+
+    jest.spyOn(accountRepositoryStub, 'findById').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const response = await sut.remove('invalid_id')
+
+    expect(response).toBeFalsy()
+  })
+
+  test('Should call delete with correct id', async () => {
+    const { sut, accountRepositoryStub } = makeSut()
+
+    const deleteSpy = jest.spyOn(accountRepositoryStub, 'delete')
+    await sut.remove('any_id')
+
+    expect(deleteSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should return true if deleted account', async () => {
+    const { sut } = makeSut()
+
+    const response = await sut.remove('any_id')
+
+    expect(response).toBeTruthy()
+  })
+})
