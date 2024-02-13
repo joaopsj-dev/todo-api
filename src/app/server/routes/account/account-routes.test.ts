597476b8a33321b286f7d18ddd5b58cddf412adb
pedrinhosj <joaopsj007@gmail.com>
2024-02-13 11:49:15 -0300

    test: ensure GET /account route operation

--- src/app/server/routes/account/account-routes.test.ts
+++ src/app/server/routes/account/account-routes.test.ts
@@ -73,4 +73,36 @@ describe('Account Routes', () => {
       .set('x-access-token', accessToken)
       .expect(200)
   }, 30000)
+
+  test('AuthMiddleware in GET /account', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+
+    await request(app)
+      .get('/api/account/any_accountId')
+      .expect(403)
+  }, 30000)
+
+  test('GET /account', async () => {
+    const { text } = await request(app)
+      .post('/api/auth/signup')
+      .send({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+
+    const { accessToken } = JSON.parse(text)
+    const { response: { payload } } = await new JwtTokenAdapter().parse(accessToken, token_protocols.accessToken_secret_key) as any
+
+    await request(app)
+      .get(`/api/account/${payload.id}`)
+      .set('x-access-token', accessToken)
+      .expect(200)
+  }, 30000)
 })
