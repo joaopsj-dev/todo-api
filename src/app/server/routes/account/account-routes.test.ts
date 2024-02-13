3169b172029a2a5694b82274ee8636fd6eeabc58
pedrinhosj <joaopsj007@gmail.com>
2024-02-13 09:57:34 -0300

    test: ensure DELETE /account route operation

--- src/app/server/routes/account/account-routes.test.ts
+++ src/app/server/routes/account/account-routes.test.ts
@@ -5,7 +5,7 @@ import token_protocols from '../../../../domain/protocols/token'
 
 describe('Account Routes', () => {
   test('AuthMiddleware in PUT /account', async () => {
-    const { text } = await request(app)
+    await request(app)
       .post('/api/auth/signup')
       .send({
         name: 'any_name',
@@ -13,11 +13,8 @@ describe('Account Routes', () => {
         password: 'any_password'
       })
 
-    const { accessToken } = JSON.parse(text)
-    const { response: { payload } } = await new JwtTokenAdapter().parse(accessToken, token_protocols.accessToken_secret_key) as any
-
     await request(app)
-      .put(`/api/account/${payload.id}`)
+      .put('/api/account/any_accountId')
       .send({
         name: 'new_name'
       })
@@ -42,6 +39,38 @@ describe('Account Routes', () => {
       .send({
         name: 'new_name'
       })
+      .expect(200)
+  }, 30000)
+
+  test('AuthMiddleware in DELETE /account', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+
+    await request(app)
+      .delete('/api/account/any_accountId')
+      .expect(403)
+  }, 30000)
+
+  test('DELETE /account', async () => {
+    const { text } = await request(app)
+      .post('/api/auth/signup')
+      .send({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+
+    const { accessToken } = JSON.parse(text)
+    const { response: { payload } } = await new JwtTokenAdapter().parse(accessToken, token_protocols.accessToken_secret_key) as any
+
+    await request(app)
+      .delete(`/api/account/${payload.id}`)
+      .set('x-access-token', accessToken)
       .expect(200)
   }, 30000)
 })
